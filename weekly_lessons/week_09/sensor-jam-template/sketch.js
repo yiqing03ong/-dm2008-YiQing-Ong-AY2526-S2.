1d128549ec0f766820203595bfe1bc6b9c6f96ed
yiqing03ong <yiqing03ong@gmail.com>
2026-04-28 00:26:45 +0800

    upload images

--- weekly_lessons/week_09/sensor-jam-template/sketch.js
+++ weekly_lessons/week_09/sensor-jam-template/sketch.js
@@ -1,15 +1,14 @@
-let port; // Serial Communication port
+let port; 
 let connectBtn;
 
 let sensorVal;
 let circleSize = 50;
-let targetSize = 50; // used for Option 2
+let targetSize = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  port = createSerial(); // creates the Serial Port
+  port = createSerial();
 
-  // Connection helpers
   connectBtn = createButton("Connect to Arduino");
   connectBtn.position(20, 20);
   connectBtn.mousePressed(connectBtnClick);
@@ -17,41 +16,31 @@ function setup() {
 
 function draw() {
   background(100);
+
   ellipse(width / 2, height / 2, circleSize);
 
   // Receive data from Arduino
   if (port.opened()) {
-    sensorVal = port.readUntil("\n");
-    // Only log data that has information, not empty signals
-    if (sensorVal[0]) {
-      // Once you verify data is coming in,
-      // disable logging to improve performance
-      console.log(sensorVal);
+    let data = port.readUntil("\n");
 
-      // OPTION 1:
-      // Update circle's size with sensor's data directly
-      // Reduce delay() value in Ardiuno to get smoother changes
+    if (data && data.length > 0) {
+      data = trim(data); // remove spaces/newlines
+      console.log(data);
 
-      // use float() to convert from data from string to number
-      // circleSize = float(sensorVal);
+      let val = float(data);
 
-      // OPTION 2:
-      // Update circle's size using lerp() to smoothly change values
-      // This method even works with longer delay() values in Arduino
-
-      targetSize = float(sensorVal);
-      // last value in lerp() controls speed of change
-      circleSize = lerp(circleSize, targetSize, 0.1);
+      if (!isNaN(val)) {
+        targetSize = val;
+        circleSize = lerp(circleSize, targetSize, 0.1);
+      }
     }
   }
 }
 
 // DO NOT REMOVE THIS FUNCTION
 function connectBtnClick(e) {
-  // If port is not already open, open on click,
-  // otherwise close the port
   if (!port.opened()) {
-    port.open(9600); // opens port with Baud Rate of 9600
+    port.open(9600);
     e.target.innerHTML = "Disconnect Arduino";
     e.target.classList.add("connected");
   } else {
